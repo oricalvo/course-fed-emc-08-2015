819b0937c63bcc4b3e8d60bcdfab1c93fb5fa9e3
oricalvo <ori.calvo@gmail.com>
2015-07-28 17:05:12 +0300

    sfdgbsdf

---
+++ Samples@class/04 jQuery/04 MyOwnSPAFramework/MyOwnSPAFramework/App.js
@@ -0,0 +1,49 @@
+﻿var MyApp = (function () {
+
+    var classes = {};
+
+    function registerClass(className, ctor) {
+        classes[className] = ctor;
+    }
+
+    function init() {
+        $("[js-class]").each(function () {
+            var element = $(this);
+            var className = element.attr("js-class");
+
+            var ctor = classes[className];
+            if (!ctor) {
+                throw new Error("Ctor " + className + " was not found");
+            }
+
+            var view = new ctor(element);
+
+            element.data("view", view);
+        });
+
+        $("[js-click]").each(function () {
+            var element = $(this);
+
+            var methodName = element.attr("js-click");
+
+            var viewElement = element.closest("[js-class]");
+            if (!viewElement.length) {
+                return;
+            }
+
+            var view = viewElement.data("view");
+
+            element.on("click", function () {
+                view[methodName]();
+            });
+        });
+    }
+
+    $(function () {
+        init();
+    });
+
+    return {
+        registerClass: registerClass,
+    };
+})();
---
+++ Samples@class/04 jQuery/04 MyOwnSPAFramework/MyOwnSPAFramework/IndexView.js
@@ -0,0 +1,16 @@
+﻿(function () {
+
+    function IndexView(element) {
+        this.element = element;
+    }
+
+    IndexView.prototype.newContact = function () {
+        alert("ddd");
+    }
+
+    IndexView.prototype.run = function () {
+        alert("xxx");
+    }
+
+    MyApp.registerClass("IndexView", IndexView);
+})();
