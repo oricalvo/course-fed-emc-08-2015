a500942a4d9e813009a8ad643823171c34a92c27
oricalvo <ori.calvo@gmail.com>
2015-08-04 16:31:23 +0300

    More

---
+++ Samples@class/06 HTML5/HistoryAPI/HistoryAPI/App.js
@@ -0,0 +1,53 @@
+﻿$(function () {
+
+    var routes = {
+    };
+
+    var currentView, defaultView;
+    defaultView = currentView = $(".view.active");
+
+    $(".view[data-url]").each(function () {
+        var view = $(this);
+
+        url = view.attr("data-url");
+
+        routes[url] = view;
+    });
+
+    //window.addEventListener("hashchange", function () {
+    //    console.log("HASHCHNAGE");
+    //});
+
+    window.addEventListener("popstate", function (e) {
+        if (!e.state) {
+            switchToView(defaultView);
+        }
+        else {
+            var view = routes[e.state.url];
+            switchToView(view);
+        }
+    });
+
+    function switchToView(view) {
+        currentView.removeClass("active");
+        view.addClass("active");
+        currentView = view;
+    }
+
+    $("body").on("click", "a", function () {
+        var link = $(this);
+
+        var url = link.attr("href");
+
+        var view = routes[url];
+        switchToView(view);
+
+        location.hash = url;
+        //history.pushState({
+        //    url: url,
+        //}, null, url);
+
+        return false;
+    });
+
+});
