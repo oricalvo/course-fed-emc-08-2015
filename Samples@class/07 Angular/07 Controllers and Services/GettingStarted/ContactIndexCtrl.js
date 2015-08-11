9713490159943fbd43bc1f48a26f89443b176d5a
oricalvo <ori.calvo@gmail.com>
2015-08-11 10:21:20 +0300

    More

---
+++ Samples@class/07 Angular/07 Controllers and Services/GettingStarted/ContactIndexCtrl.js
@@ -0,0 +1,32 @@
+﻿(function () {
+
+    function ContactIndexCtrl($scope, contactService) {
+        $scope.contacts = contactService.getAll().map(function (contact) {
+            return {
+                name: contact.name,
+                selected: false,
+            };
+        });
+
+        $scope.$on("contactAdded", function (e, contact) {
+            $scope.contacts.push({
+                name: contact.name,
+                selected: false,
+            });
+        });
+
+        $scope.delete = function () {
+            var count = 0;
+
+            $scope.contacts.forEach(function (contact) {
+                count += (contact.selected * 1);
+            });
+
+            confirm("Delete " + count + " contacts ?", function () {
+            });
+        }
+    }
+
+    angular.module("MyApp").controller("ContactIndexCtrl", ContactIndexCtrl);
+
+})();
---
+++ Samples@class/07 Angular/07 Controllers and Services/GettingStarted/ContactNewCtrl.js
@@ -0,0 +1,16 @@
+﻿(function () {
+
+    function ContactNewCtrl($scope, contactService) {
+        $scope.save = function () {
+            if (!$scope.name) {
+                return;
+            }
+
+            var contact = { id: -1, name: $scope.name };
+            contactService.add(contact);
+        }
+    }
+
+    angular.module("MyApp").controller("ContactNewCtrl", ContactNewCtrl);
+
+})();
